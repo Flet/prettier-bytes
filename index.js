a9a7734086d0bdba1c0747f1ca25cbfca91148cf
Feross Aboukhadijeh <feross@feross.org>
2016-03-08 13:27:26 -0800

    - rounding happens correctly for all sizes now - removed string hacks. handle decimals issue directly

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
 module.exports = prettierBytes
 
-function prettierBytes (b) {
-  var pretty = prettyBytes(b)
-
-  var ps = pretty.split(/\.| /)
-  // If there is more than one digit to the left of the decimal, chop off the
-  // fractional part.
-  if (ps.length > 2 && ps[0].replace('-', '').length > 1) {
-    return ps[0] + ' ' + ps[2]
-  }
-
-  return ps[0] + '.' + ps[1] + ' ' + ps[2]
-}
-
-function prettyBytes (num) {
+function prettierBytes (num) {
   if (typeof num !== 'number' || Number.isNaN(num)) {
     throw new TypeError('Expected a number, got ' + typeof num)
   }
@@ -30,8 +17,14 @@ function prettyBytes (num) {
   }
 
   var exponent = Math.min(Math.floor(Math.log(num) / Math.log(1000)), units.length - 1)
-  num = Number((num / Math.pow(1000, exponent)).toFixed(1))
+  num = Number(num / Math.pow(1000, exponent))
   var unit = units[exponent]
 
-  return (neg ? '-' : '') + num + ' ' + unit
+  if (num >= 10 || num % 1 === 0) {
+    // Do not show decimals when the number is two-digit, or if the number has no
+    // decimal component.
+    return (neg ? '-' : '') + num.toFixed(0) + ' ' + unit
+  } else {
+    return (neg ? '-' : '') + num.toFixed(1) + ' ' + unit
+  }
 }
