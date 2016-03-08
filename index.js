9cb2454a390c97debe0bf00f7f5fe9bffab9022e
Feross Aboukhadijeh <feross@feross.org>
2016-03-08 13:14:49 -0800

    Lots of improvements
    - inline pretty-bytes implementation so we can change it
    - Change kB to KB, for prettiness. Technical accuracy doesn't matter.
    See my comment here:
    https://github.com/sindresorhus/pretty-bytes/issues/17#issuecomment-1939
    66059
    - Round up (use toFixed(1), instead of toFixed(2) and chopping off the
    last digit)
    - remove `number-is-nan` dependency
    - add throw tests

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
-var prettyBytes = require('pretty-bytes')
-
 module.exports = prettierBytes
 
-// prettier bytes, less verbose
-// str -> str
 function prettierBytes (b) {
   var pretty = prettyBytes(b)
 
   var ps = pretty.split(/\.| /)
-  // if there is more than one digit to the left of the decimal, chop off the
+  // If there is more than one digit to the left of the decimal, chop off the
   // fractional part.
   if (ps.length > 2 && ps[0].replace('-', '').length > 1) {
     return ps[0] + ' ' + ps[2]
-  } else {
-    return ps[0] + '.' + ps[1].charAt(0) + ' ' + ps[2]
   }
+
+  return ps[0] + '.' + ps[1] + ' ' + ps[2]
+}
+
+function prettyBytes (num) {
+  if (typeof num !== 'number' || Number.isNaN(num)) {
+    throw new TypeError('Expected a number, got ' + typeof num)
+  }
+
+  var neg = num < 0
+  var units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+
+  if (neg) {
+    num = -num
+  }
+
+  if (num < 1) {
+    return (neg ? '-' : '') + num + ' B'
+  }
+
+  var exponent = Math.min(Math.floor(Math.log(num) / Math.log(1000)), units.length - 1)
+  num = Number((num / Math.pow(1000, exponent)).toFixed(1))
+  var unit = units[exponent]
+
+  return (neg ? '-' : '') + num + ' ' + unit
 }
--- test/index.js
+++ test/index.js
@@ -3,16 +3,16 @@ var test = require('tape')
 var prettierBytes = require('../')
 
 var testData = [
-  [2335, '2.3 kB'],
-  [23552, '23 kB'],
-  [235520, '235 kB'],
-  [2355520, '2.3 MB'],
+  [2335, '2.3 KB'],
+  [23552, '23 KB'],
+  [235520, '235 KB'],
+  [2355520, '2.4 MB'],
   [23555520, '23 MB'],
   [235555520, '235 MB'],
-  [2355555520, '2.3 GB'],
+  [2355555520, '2.4 GB'],
   [23555555520, '23 GB'],
   [235556555520, '235 GB'],
-  [2355556655520, '2.3 TB'],
+  [2355556655520, '2.4 TB'],
   [23555566655520, '23 TB'],
   [235555566665520, '235 TB']
 ]
@@ -22,5 +22,19 @@ test('pretty bytes', function (t) {
     t.equals(prettierBytes(data[0]), data[1], 'bytes: ' + data[0] + ' -> ' + data[1])
   })
 
+  t.end()
+})
+
+test('throws on non-number', function (t) {
+  t.throws(function () {
+    prettierBytes('this is a string')
+  })
+  t.end()
+})
+
+test('throws on NaN', function (t) {
+  t.throws(function () {
+    prettierBytes(NaN)
+  })
   t.end()
 })
